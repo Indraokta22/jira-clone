8d4e848773e577bd9b61dc1ee98bb3a0bd1b4375
Indra Oktafiyanto <indraoktafiyanto17@gmail.com>
2025-02-16 14:37:34 +0700

    refactoring & settings added

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,11 +1,11 @@
-import { getCurrent } from "@/features/auth/actions";
-import { getWorkspaces } from "@/features/workspaces/actions";
+import { getCurrent } from "@/features/auth/queries";
+import { getWorkspaces } from "@/features/workspaces/queries";
 import { redirect } from "next/navigation";
 
 export default async function Home() {
   const user = await getCurrent();
 
-  if(!user) redirect("/sign-in");
+  if (!user) redirect("/sign-in");
 
   const workspaces = await getWorkspaces();
   if (workspaces.total === 0) {

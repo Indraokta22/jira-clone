acbdb9cba5b45931dc3c23a2d6845725eb2cab90
Indra Oktafiyanto <indraoktafiyanto17@gmail.com>
2025-02-20 22:37:07 +0700

    project edit & delete added

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -11,7 +11,6 @@ import { ProjectAvatar } from "@/features/projects/components/project-avatar";
 
 
 export const Projects = () => {
-    const projectId = null; //TODO: use projectId hook
 
     const pathname = usePathname();
     const { open } = useCreateProjectModal();
@@ -27,7 +26,7 @@ export const Projects = () => {
                 <RiAddCircleFill onClick={open} className="size-5 text-neutral-500 cursor-pointer hover:opacity-75 transition" />
             </div>
             {data?.documents.map((project) => {
-                const href = `/workspace/${workspaceId}/projects/${projectId}`
+                const href = `/workspaces/${workspaceId}/projects/${project.$id}`
                 const isActive = pathname === href;
 
                 return (

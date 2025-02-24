beff56b3b44ee4af779fad368180e41b5a0dc341
Indra Oktafiyanto <indraoktafiyanto17@gmail.com>
2025-02-25 01:25:05 +0700

    rafactoring

--- src/features/workspaces/components/edit-workspace-form.tsx
+++ src/features/workspaces/components/edit-workspace-form.tsx
@@ -92,10 +92,6 @@ export const EditWorkspaceForm = ({ onCancel, initialValues }: EditWorkspaceForm
         mutate({
             form: finalValues,
             param: { workspaceId: initialValues.$id }
-        }, {
-            onSuccess: () => {
-                form.reset();
-            }
         });
     };
 
